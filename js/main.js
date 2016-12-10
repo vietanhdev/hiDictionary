fe12cacf2fca4af315a0bd86e031a6ec66b9b25b
Viet Anh Nguyen <vietanh@vietanhdev.com>
2016-12-10 21:14:01 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function() {
+
+
+    resizeSearchBox();
+
+    function resizeSearchBox() {
+        let searchBoxWidth = $('.input-area').innerWidth() - $('.input-area .searchOptions').outerWidth() - $('.input-area .search-btn').outerWidth() - 42 + 'px'
+        $('.input-area .searchBox').width(searchBoxWidth);
+    }
+
+    $(window).resize(function() {
+        resizeSearchBox();
+    });
+
+});
